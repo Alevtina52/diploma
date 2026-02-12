3f6c245b0d24f7837d9bf2a65ae528f8af2e10da
Alevtina52 <fedals78@gmail.com>
2026-02-12 18:41:21 +0700

    Авторизация/Выход

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DistrictController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/hash', function () {
+    return bcrypt('123456');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/map', [DistrictController::class, 'index'])->name('map');
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+
+});
+
